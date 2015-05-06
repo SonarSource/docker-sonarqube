db8c43301fd26fe5653a93f9deb5ade107e87d25
David Gageot <david@gageot.net>
2015-05-06 10:15:48 +0200

    Support lts version

---
+++ 4.5.4/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+echo Running SonarQube [$SONAR_VERSION]
+
+cd $SONAR_HOME
+
+java -jar lib/sonar-application-$SONAR_VERSION.jar \
+  -Dsonar.log.console=true \
+  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
+  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
+  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
+  -Dsonar.web.javaAdditionalOpts="-Djava.security.egd=file:/dev/./urandom"
