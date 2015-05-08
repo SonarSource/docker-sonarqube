2e20cb93d8edaf09f7c57b378fa1fba424642eb6
David Gageot <david@gageot.net>
2015-05-08 22:36:12 +0200

    Useless

--- 5.1/run.sh
+++ 5.1/run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-set -euo pipefail
-IFS=$'\n\t'
-
-echo Running SonarQube [$SONAR_VERSION]
-
 exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.log.console=true \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
