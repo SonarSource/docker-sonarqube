e09a66a5a2e6337e2852c4f36659632ea505fdb1
David Gageot <david@gageot.net>
2015-08-02 07:01:19 +0200

    Add SonarQube 4.5.5

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker build -t sonarsource/sonar-4.5.4 4.5.4
+docker build -t sonarsource/sonar-4.5.5 4.5.5
 docker build -t sonarsource/sonar-5.1 5.1
 docker build -t sonarsource/sonar-5.1.1 5.1.1
