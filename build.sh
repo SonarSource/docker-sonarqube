e3500f26e706b239e43bc7b5b2712ac7e4d58974
Evgeny Mandrikov <mandrikov@gmail.com>
2015-11-04 18:09:05 +0100

    Add SonarQube 4.5.6

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 docker build -t sonarsource/sonar-4.5.4 4.5.4
 docker build -t sonarsource/sonar-4.5.5 4.5.5
+docker build -t sonarsource/sonar-4.5.6 4.5.6
 docker build -t sonarsource/sonar-5.1 5.1
 docker build -t sonarsource/sonar-5.1.1 5.1.1
 docker build -t sonarsource/sonar-5.1.2 5.1.2
