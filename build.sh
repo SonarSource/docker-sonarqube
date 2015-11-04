f17d143b8bb45775897e25a6cbafe1a2d832c206
Eddú Meléndez <eddu.melendez@gmail.com>
2015-11-03 23:22:56 -0500

    Add SonarQube 5.2 & 4.5.6

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 docker build -t sonarsource/sonar-4.5.4 4.5.4
 docker build -t sonarsource/sonar-4.5.5 4.5.5
+docker build -t sonarsource/sonar-4.5.6 4.5.6
 docker build -t sonarsource/sonar-5.1 5.1
 docker build -t sonarsource/sonar-5.1.1 5.1.1
 docker build -t sonarsource/sonar-5.1.2 5.1.2
+docker build -t sonarsource/sonar-5.2 5.2
