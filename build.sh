1cdb499b298ac3fb26d18d7041f15ebe267905b9
Evgeny Mandrikov <mandrikov@gmail.com>
2016-01-11 21:58:20 +0100

    Remove minor versions

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 
 set -euo pipefail
 
-docker build -t sonarsource/sonar-4.5.4 4.5.4
-docker build -t sonarsource/sonar-4.5.5 4.5.5
 docker build -t sonarsource/sonar-4.5.6 4.5.6
-docker build -t sonarsource/sonar-5.1 5.1
-docker build -t sonarsource/sonar-5.1.1 5.1.1
 docker build -t sonarsource/sonar-5.1.2 5.1.2
 docker build -t sonarsource/sonar-5.2 5.2
