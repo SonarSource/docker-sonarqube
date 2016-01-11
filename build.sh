f13192265920c40f8a50ec55f7c2af7a412c4c88
Evgeny Mandrikov <mandrikov@gmail.com>
2016-01-11 20:00:05 +0100

    Add SonarQube 5.3

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ set -euo pipefail
 docker build -t sonarsource/sonar-4.5.6 4.5.6
 docker build -t sonarsource/sonar-5.1.2 5.1.2
 docker build -t sonarsource/sonar-5.2 5.2
+docker build -t sonarsource/sonar-5.3 5.3
