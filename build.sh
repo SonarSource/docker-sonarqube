f8baccb64f41ca126781db6a700d738d004fd0b0
Evgeny Mandrikov <mandrikov@gmail.com>
2016-04-08 17:38:13 +0200

    Update SonarQube LTS to 4.5.7

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-docker build -t sonarsource/sonar-4.5.6 4.5.6
+docker build -t sonarsource/sonar-4.5.7 4.5.7
 docker build -t sonarsource/sonar-5.1.2 5.1.2
 docker build -t sonarsource/sonar-5.2 5.2
 docker build -t sonarsource/sonar-5.3 5.3
