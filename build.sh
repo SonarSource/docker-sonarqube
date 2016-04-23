1d696886185a5f8aa345b3040153ba92769c34e4
Evgeny Mandrikov <mandrikov@gmail.com>
2016-04-23 15:28:36 +0200

    Add images based on Alpine Linux

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ docker build -t sonarsource/sonar-5.1.2 5.1.2
 docker build -t sonarsource/sonar-5.2 5.2
 docker build -t sonarsource/sonar-5.3 5.3
 docker build -t sonarsource/sonar-5.4 5.4
+
+docker build -t sonarsource/sonar-4.5.7-alpine 4.5.7-alpine
+docker build -t sonarsource/sonar-5.4-alpine 5.4-alpine
