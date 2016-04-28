370771b3af62f84db4e823ad2f61a1b6c1817821
Evgeny Mandrikov <mandrikov@gmail.com>
2016-04-28 23:56:47 +0200

    Should be possible to specify additional options for Web JVM

--- 4.5.7-alpine/run.sh
+++ 4.5.7-alpine/run.sh
@@ -11,5 +11,5 @@ exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
   -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
   -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
-  -Dsonar.web.javaAdditionalOpts="-Djava.security.egd=file:/dev/./urandom" \
+  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
   "$@"
