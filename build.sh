97f3dddfdd91b6f444be8e4275d33a8102a8718f
Evgeny Mandrikov <mandrikov@gmail.com>
2016-05-02 20:13:11 +0200

    Remove old intermediate versions
    They are not published at https://hub.docker.com/_/sonarqube

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@
 set -euo pipefail
 
 docker build -t sonarsource/sonar-4.5.7 4.5.7
-docker build -t sonarsource/sonar-5.1.2 5.1.2
-docker build -t sonarsource/sonar-5.2 5.2
-docker build -t sonarsource/sonar-5.3 5.3
 docker build -t sonarsource/sonar-5.4 5.4
 
 docker build -t sonarsource/sonar-4.5.7-alpine 4.5.7-alpine
