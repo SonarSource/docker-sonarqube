c0cae340838f57fb2af7b6441daaf3d2987a4d34
Evgeny Mandrikov <mandrikov@gmail.com>
2017-06-02 14:56:27 +0200

    SonarQube 6.4

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 docker build -t sonarsource/sonar-5.6.6 5.6.6
 docker build -t sonarsource/sonar-5.6.6-alpine 5.6.6-alpine
-docker build -t sonarsource/sonar-6.3.1 6.3.1
-docker build -t sonarsource/sonar-6.3.1-alpine 6.3.1-alpine
+docker build -t sonarsource/sonar-6.4 6.4
+docker build -t sonarsource/sonar-6.4-alpine 6.4-alpine
