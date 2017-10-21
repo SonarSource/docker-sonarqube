8b1d6f149b0445b4c688085930528b67842d7349
Evgeny Mandrikov <mandrikov@gmail.com>
2017-10-21 12:48:49 +0200

    Elasticsearch in SonarQube 6.6 requires non-root user and bash

--- 6.6-alpine/run.sh
+++ 6.6-alpine/run.sh
@@ -6,7 +6,8 @@ if [ "${1:0:1}" != '-' ]; then
   exec "$@"
 fi
 
-exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
+exec su-exec sonarqube \
+  java -jar lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.log.console=true \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
   -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
