a53608bbcc03c7f0c19805eaa69c1f9088663f84
Evgeny Mandrikov <mandrikov@gmail.com>
2017-12-26 16:39:39 +0100

    Use matrix in Travis

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-docker build -t sonarsource/sonar-5.6.7 5.6.7
-docker build -t sonarsource/sonar-5.6.7-alpine 5.6.7-alpine
-docker build -t sonarsource/sonar-6.7 6.7
-docker build -t sonarsource/sonar-6.7-alpine 6.7-alpine
