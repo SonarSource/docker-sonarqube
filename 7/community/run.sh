55d96a8d92a0eab8a978e26f01ccfa967d86421e
Adam Gerson <adgerson@users.noreply.github.com>
2019-08-15 16:30:07 -0700

    Add env support Docker secrets
    Setting sonar.jdbc.password.file
    will fill in the value of "$sonar.jdbc.password"
    from a file for Docker's secrets feature

--- 7/community/run.sh
+++ 7/community/run.sh
@@ -11,14 +11,19 @@ fi
 # e.g. Setting the env var sonar.jdbc.username=foo
 #
 # will cause SonarQube to be invoked with -Dsonar.jdbc.username=foo
+#
+# Setting sonar.jdbc.password.file will fill in the value of
+# "$sonar.jdbc.password" from a file for Docker's secrets feature
 
 declare -a sq_opts
 
 while IFS='=' read -r envvar_key envvar_value
 do
-    if [[ "$envvar_key" =~ sonar.* ]] || [[ "$envvar_key" =~ ldap.* ]]; then
-        sq_opts+=("-D${envvar_key}=${envvar_value}")
-    fi
+  if [[ "$envvar_key" =~ sonar.*.file ]] || [[ "$envvar_key" =~ ldap.*.file ]]; then
+    sq_opts+=("-D${envvar_key%.file}=$(<"$envvar_value")")
+  elif [[ "$envvar_key" =~ sonar.* ]] || [[ "$envvar_key" =~ ldap.* ]]; then
+    sq_opts+=("-D${envvar_key}=${envvar_value}")
+  fi
 done < <(env)
 
 exec tail -F ./logs/es.log & # this tail on the elasticsearch logs is a temporary workaround, see https://github.com/docker-library/official-images/pull/6361#issuecomment-516184762
