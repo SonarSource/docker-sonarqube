cded412855667ed8762f60a7bd790a36eb0af4ac
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2019-09-30 14:35:30 +0200

    [#323] enable unofficial strict bash mode on run.sh

--- 8/enterprise/run.sh
+++ 8/enterprise/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -e
+set -eou pipefail
 
-if [ "${1:0:1}" != '-' ]; then
+if [[ "${1:-}" != -* ]]; then
   exec "$@"
 fi
 
@@ -11,9 +11,7 @@ fi
 # e.g. Setting the env var sonar.jdbc.username=foo
 #
 # will cause SonarQube to be invoked with -Dsonar.jdbc.username=foo
-
 declare -a sq_opts
-
 while IFS='=' read -r envvar_key envvar_value
 do
     if [[ "$envvar_key" =~ sonar.* ]] || [[ "$envvar_key" =~ ldap.* ]]; then
@@ -21,11 +19,11 @@ do
     fi
 done < <(env)
 
-exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
+exec java -jar "lib/sonar-application-$SONAR_VERSION.jar" \
   -Dsonar.log.console=true \
-  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
-  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
-  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
-  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
+  -Dsonar.jdbc.username="${SONARQUBE_JDBC_USERNAME:-}" \
+  -Dsonar.jdbc.password="${SONARQUBE_JDBC_PASSWORD:-}" \
+  -Dsonar.jdbc.url="${SONARQUBE_JDBC_URL:-}" \
+  -Dsonar.web.javaAdditionalOpts="${SONARQUBE_WEB_JVM_OPTS:-} -Djava.security.egd=file:/dev/./urandom" \
   "${sq_opts[@]}" \
   "$@"
