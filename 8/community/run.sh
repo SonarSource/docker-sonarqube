29d8089232433be2d5381d791234c4d6f325b8e6
Sébastien Lesaint <sns-seb@users.noreply.github.com>
2019-10-01 16:26:35 +0200

    MMF-1604 Harden volume support (#320)
    * [#322]MMF-1604 initialize mounted volumes when they are empty
    
    DockerFile:
     - move unpacked SQ archive out of /opt/sonarqube directory and use symlinks to /opt/sonarqube mounted directories
     - make it possible to copy conf and extensions dir in run.sh by keeping a copy in conf_save and extensions_save
     - logically group RUN commands
     - make user sonarqube owner of run.sh
     - add comments
    
    run.sh
     - initialize conf and extensions directories with content from unpacked artifact if they are empty
     - display error message without failing the script if initialization fails, other message when succeeds

--- 8/community/run.sh
+++ 8/community/run.sh
@@ -2,8 +2,15 @@
 
 set -e
 
+init_only=false
+SONARQUBE_HOME=/opt/sq 
+
 if [ "${1:0:1}" != '-' ]; then
   exec "$@"
+fi
+
+if [ "${1:-}" = "--init" ]; then
+  init_only=true
 fi
 
 # Parse Docker env vars to customize SonarQube
@@ -21,11 +28,33 @@ do
     fi
 done < <(env)
 
-exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
-  -Dsonar.log.console=true \
-  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
-  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
-  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
-  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
-  "${sq_opts[@]}" \
-  "$@"
+is_empty_dir() {
+  [ -z "$(ls -A "$1")" ]
+}
+
+initialize_sq_sub_dir() {
+  local sub_dir="$1"
+
+  if is_empty_dir "$SONARQUBE_HOME/${sub_dir}"; then
+    cp --recursive "$SONARQUBE_HOME/${sub_dir}_save/." "$SONARQUBE_HOME/${sub_dir}/" \
+      && echo "Initialized content of $SONARQUBE_HOME/${sub_dir}" \
+      || echo "Failed to initialize content of $SONARQUBE_HOME/${sub_dir}"
+  fi
+}
+
+# Initialize conf and extensions dir in case they have been bound to a Docker Daemon host's filesystem directory
+# or to an empty volumne which has been created prior to the 'docker run' command call
+# Initialization only occurs if directory is totally empty
+initialize_sq_sub_dir "conf"
+initialize_sq_sub_dir "extensions"
+
+if [ "$init_only" = false ]; then
+  exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
+    -Dsonar.log.console=true \
+    -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
+    -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
+    -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
+    -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
+    "${sq_opts[@]}" \
+    "$@"
+fi
