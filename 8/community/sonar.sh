db5225f43c0d29e5aca482681aebc382cc4eb28d
Michal Duda <michal.duda@sonarsource.com>
2020-04-15 14:42:27 +0200

    apply DockerHub review feedback

--- 8/community/sonar.sh
+++ 8/community/sonar.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec java -jar "lib/sonar-application-$SONAR_VERSION.jar" -Dsonar.log.console=true $@
+exec java -jar lib/sonar-application-"${SONAR_VERSION}".jar -Dsonar.log.console=true "$@"
