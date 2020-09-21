75042426d27c24e3d96cf001ce0e1c2245ce26ef
Shubham Garg <shubhamgargcth@gmail.com>
2020-09-21 10:11:17 +0530

    Add option to disable the es bootstrap checks
    Currently sonar qube can not be hosted in azure app service as it gives vm.max_map_count error. There is already a flag to disable the bootstrap checks but azure app service replaced "." with "-" so there is no way currently to set the flag sonar.es.bootstrap.checks.disable from app service. With this changes user will be able to set the env variable value from app settting.

--- 7/community/run.sh
+++ 7/community/run.sh
@@ -27,6 +27,7 @@ exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
   -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
   -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
+  -Dsonar.es.bootstrap.checks.disable="$SONAR_ES_BOOTSTRAP_CHECKS_DISABLE" \
   -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
   "${sq_opts[@]}" \
   "$@"
