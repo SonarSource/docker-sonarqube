dd5368b78c5dc21bdef86b6c6b42108a17db8458
Guilhem Bonnefille <guilhem.bonnefille@csgroup.eu>
2020-10-12 09:52:49 +0200

    Check presence of SONARQUBE_*
    When SONARQUBE_* are not set, previous implementation pass -D options with empty values.
    Doing this, any values set in the property file are ignored.
    
    This patch avoid setting -D options when var env are not set.

--- 7/community/run.sh
+++ 7/community/run.sh
@@ -21,12 +21,22 @@ do
     fi
 done < <(env)
 
+if [ -n "$SONARQUBE_JDBC_USERNAME" ]
+then
+    sq_opts+=("-Dsonar.jdbc.username='$SONARQUBE_JDBC_USERNAME'")
+fi
+if [ -n "$SONARQUBE_JDBC_PASSWORD" ]
+then
+    sq_opts+=("-Dsonar.jdbc.password='$SONARQUBE_JDBC_PASSWORD'")
+fi
+if [ -n "$SONARQUBE_JDBC_URL" ]
+then
+    sq_opts+=("-Dsonar.jdbc.url='$SONARQUBE_JDBC_URL'")
+fi
+
 exec tail -F ./logs/es.log & # this tail on the elasticsearch logs is a temporary workaround, see https://github.com/docker-library/official-images/pull/6361#issuecomment-516184762
 exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.log.console=true \
-  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
-  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
-  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
   -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
   "${sq_opts[@]}" \
   "$@"
