cf2d3f8876db882b3e477f8cf884241e9322571f
tobias-trabelsi-sonarsource <tobias.trabelsi@sonarsource.com>
2021-02-01 11:00:57 +0100

    started work on dce images

---
+++ 8/datacenter/search/sonar.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+IP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+exec java -jar lib/sonar-application-"${SONAR_VERSION}".jar -Dsonar.log.console=true -Dsonar.log.console=true -Dsonar.cluster.node.search.host=${IP} -Dsonar.cluster.node.es.host=${IP} -Dsonar.cluster.node.host=${IP} "$@"
