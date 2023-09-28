7a88eafcb071b103daffbffb95a49d4f7220124a
Carmine Vassallo <carmine.vassallo@sonarsource.com>
2023-09-28 10:14:14 +0200

    SONAR-20383 Add retry mechanism for multi-architecture docker build

---
+++ .cirrus/multi-arch-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export DOCKER_BUILDKIT=1
+for i in $(seq 1 3); do
+  if docker buildx build --platform linux/amd64,linux/arm64 --tag "$1:$2" --push $3; then
+    echo "[Success]: Buildx, attempt ${i}"
+    exit 0
+  fi
+  sleep 3
+done
+echo "[Error]: Context Deadline Exceeded - Buildx"
+exit 1
