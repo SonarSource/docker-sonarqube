0cd256fd168ee4f729bc87b6259c7d9eb1349400
Davi Koscianski Vidal <davi.koscianski-vidal@sonarsource.com>
2023-10-10 09:54:36 +0200

    SONAR-20590 Add retry mechanism for public and private scan tasks

---
+++ .cirrus/pull.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IMAGE_NAME=${1}
+tag=${2}
+platform=${3}
+
+for i in $(seq 1 3); do
+    if docker pull --platform linux/"${platform}" "${IMAGE_NAME}:${tag}"; then
+        exit 0
+    fi
+    echo "[${i}/3] Retrying to pull image ${IMAGE_NAME}:${tag}..."
+    sleep 5
+done
+echo "[Error]: Failed to pull image ${IMAGE_NAME}:${tag}"
+exit 1
