e87d3291b4c7eb488dcf80f37103ede0bd6c8c26
sonal-chauhan <sonal.chauhan@grofers.com>
2024-01-16 17:35:48 +0530

    configmaps

--- 10/community/entrypoint.sh
+++ 10/community/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-DEFAULT_CMD=('/opt/java/openjdk/bin/java' '-jar' 'lib/sonarqube.jar' '-Dsonar.log.console=true')
+export SONAR_JDBC_URL=$(yq .SONAR_JDBC_URL /configs/config.yaml )
+export SONAR_JDBC_USERNAME=$(yq .SONAR_JDBC_USERNAME /configs/config.yaml )
+export SONAR_JDBC_PASSWORD=$(yq .SONAR_JDBC_PASSWORD /configs/config.yaml )
+
+DEFAULT_CMD=('/opt/java/openjdk/bin/java' '-jar' 'lib/sonarqube.jar' '-Dsonar.log.console=true' "-Dsonar.jdbc.url=${SONAR_JDBC_URL}" "-Dsonar.jdbc.username=${SONAR_JDBC_USERNAME}" "-Dsonar.jdbc.password=${SONAR_JDBC_PASSWORD}")
 
 # this if will check if the first argument is a flag
 # but only works if all arguments require a hyphenated flag
