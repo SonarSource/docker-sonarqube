7caa50373c4e5a97aee478d47f61f5777e5869fe
Carmine Vassallo <carmine.vassallo@sonarsource.com>
2025-02-06 11:03:47 +0100

    SONAR-24220 Set a new GCP-Service-name annotation

--- .cirrus/multi-arch-build.sh
+++ .cirrus/multi-arch-build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 export DOCKER_BUILDKIT=1
+EXTRA_DOCKER_BUILD_ARGS=${EXTRA_DOCKER_BUILD_ARGS:-""}
+
+echo "Using the following extra args: ${EXTRA_DOCKER_BUILD_ARGS}"
+
 for i in $(seq 1 3); do
-  if docker buildx build --platform linux/amd64,linux/arm64 --tag "$1:$2" --push $3; then
+  if docker buildx build --platform linux/amd64,linux/arm64 ${EXTRA_DOCKER_BUILD_ARGS} --tag "$1:$2" --push $3; then
     echo "[Success]: Buildx, attempt ${i}"
     exit 0
   fi
