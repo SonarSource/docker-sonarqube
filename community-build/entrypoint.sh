8c5d0a01f220daf7464926135693b04a3955c9a4
Jérémy Cotineau <jeremy.cotineau@sonarsource.com>
2025-10-06 16:47:47 +0200

    SONAR-25955 fix overhall quality gate

--- community-build/entrypoint.sh
+++ community-build/entrypoint.sh
@@ -6,7 +6,7 @@ DEFAULT_CMD=('/opt/java/openjdk/bin/java' '-jar' 'lib/sonarqube.jar' '-Dsonar.lo
 # this if will check if the first argument is a flag
 # but only works if all arguments require a hyphenated flag
 # -v; -SL; -f arg; etc will work, but not arg1 arg2
-if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
+if [[ $# -eq 0 ]] || [[ "${1#-}" != "${1}" ]]; then
     set -- "${DEFAULT_CMD[@]}" "$@"
 fi
 
