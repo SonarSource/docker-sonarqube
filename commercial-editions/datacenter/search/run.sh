f3c6cff40400714588bb924fb086d39400b95fbb
Carmine Vassallo <carmine.vassallo@sonarsource.com>
2026-03-02 18:10:43 +0100

    Fix conditional tests in Bash scripts

--- commercial-editions/datacenter/search/run.sh
+++ commercial-editions/datacenter/search/run.sh
@@ -7,18 +7,18 @@ IP=$(ip -4 address show scope global | grep inet | awk '{ print $2 }' | head -n 
 
 declare -a sq_opts=()
 set_prop() {
-  if [ "$2" ]; then
+  if [[ "$2" ]]; then
     sq_opts+=("-D$1=$2")
   fi
 }
 
 # if nothing is passed, assume we want to run sonarqube server
-if [ "$#" == 0 ]; then
+if [[ "$#" == 0 ]]; then
   set -- /opt/sonarqube/docker/sonar.sh
 fi
 
 # if first arg looks like a flag, assume we want to run sonarqube server with flags
-if [ "${1:0:1}" = '-' ]; then
+if [[ "${1:0:1}" = '-' ]]; then
     set -- /opt/sonarqube/docker/sonar.sh "$@"
 fi
 
@@ -30,7 +30,7 @@ if [[ "$1" = '/opt/sonarqube/docker/sonar.sh' ]]; then
     set_prop "sonar.cluster.node.search.host" "${IP:-}"
     set_prop "sonar.cluster.node.es.host" "${IP:-}"
 
-    if [ ${#sq_opts[@]} -ne 0 ]; then
+    if [[ ${#sq_opts[@]} -ne 0 ]]; then
         set -- "$@" "${sq_opts[@]}"
     fi
 fi
