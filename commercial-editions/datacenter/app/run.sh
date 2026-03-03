fca7ae69a2fc313fcfad33e11ecb7a81b85a3d76
Carmine Vassallo <carmine.vassallo@sonarsource.com>
2026-03-03 09:56:50 +0100

    SONAR-27204 Fix positional parameters and conditional tests in Bash scripts

--- commercial-editions/datacenter/app/run.sh
+++ commercial-editions/datacenter/app/run.sh
@@ -7,18 +7,23 @@ IP=$(ip -4 address show scope global | grep inet | awk '{ print $2 }' | head -n 
 
 declare -a sq_opts=()
 set_prop() {
-  if [ "$2" ]; then
-    sq_opts+=("-D$1=$2")
+  local name="$1"
+  local value="$2"
+
+  if [[ "$value" ]]; then
+    sq_opts+=("-D$name=$value")
   fi
+
+  return 0
 }
 
 # if nothing is passed, assume we want to run sonarqube server
-if [ "$#" == 0 ]; then
+if [[ "$#" == 0 ]]; then
   set -- /opt/sonarqube/docker/sonar.sh
 fi
 
 # if first arg looks like a flag, assume we want to run sonarqube server with flags
-if [ "${1:0:1}" = '-' ]; then
+if [[ "${1:0:1}" = '-' ]]; then
     set -- /opt/sonarqube/docker/sonar.sh "$@"
 fi
 
@@ -28,7 +33,7 @@ if [[ "$1" = '/opt/sonarqube/docker/sonar.sh' ]]; then
     # Change log path to ensure every app node can write in their own directory
     # This resolves a cluttered log on docker-compose with scale > 1
     #
-    if [ -z "${SONAR_PATH_LOGS:-}" ]
+    if [[ -z "${SONAR_PATH_LOGS:-}" ]]
     then
         SONAR_CLUSTER_PATH_LOGS="logs/${HOSTNAME}"
         mkdir -p ${SONARQUBE_HOME}/${SONAR_CLUSTER_PATH_LOGS}
@@ -42,7 +47,7 @@ if [[ "$1" = '/opt/sonarqube/docker/sonar.sh' ]]; then
     #
     set_prop "sonar.cluster.node.host" "${IP:-}"
     set_prop "sonar.path.logs" "${SONAR_CLUSTER_PATH_LOGS:-}"
-    if [ ${#sq_opts[@]} -ne 0 ]; then
+    if [[ ${#sq_opts[@]} -ne 0 ]]; then
         set -- "$@" "${sq_opts[@]}"
     fi
 fi
--- commercial-editions/datacenter/search/run.sh
+++ commercial-editions/datacenter/search/run.sh
@@ -7,18 +7,23 @@ IP=$(ip -4 address show scope global | grep inet | awk '{ print $2 }' | head -n 
 
 declare -a sq_opts=()
 set_prop() {
-  if [ "$2" ]; then
-    sq_opts+=("-D$1=$2")
+  local name="$1"
+  local value="$2"
+
+  if [[ "$value" ]]; then
+    sq_opts+=("-D$name=$value")
   fi
+
+  return 0
 }
 
 # if nothing is passed, assume we want to run sonarqube server
-if [ "$#" == 0 ]; then
+if [[ "$#" == 0 ]]; then
   set -- /opt/sonarqube/docker/sonar.sh
 fi
 
 # if first arg looks like a flag, assume we want to run sonarqube server with flags
-if [ "${1:0:1}" = '-' ]; then
+if [[ "${1:0:1}" = '-' ]]; then
     set -- /opt/sonarqube/docker/sonar.sh "$@"
 fi
 
@@ -30,7 +35,7 @@ if [[ "$1" = '/opt/sonarqube/docker/sonar.sh' ]]; then
     set_prop "sonar.cluster.node.search.host" "${IP:-}"
     set_prop "sonar.cluster.node.es.host" "${IP:-}"
 
-    if [ ${#sq_opts[@]} -ne 0 ]; then
+    if [[ ${#sq_opts[@]} -ne 0 ]]; then
         set -- "$@" "${sq_opts[@]}"
     fi
 fi
